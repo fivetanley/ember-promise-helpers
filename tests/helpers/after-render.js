27a15c95d626db4f93158426d062572615fb5404
Jose Luis Represa <joseluis.represa@bbva.com>
2017-04-05 13:23:25 +0200

    fixed tests for Ember release, helper value should be checked in the next run loop

--- tests/helpers/after-render.js
+++ tests/helpers/after-render.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default function afterRender(promise) {
   return promise.catch(() => {}).finally(() => {
     return new Ember.RSVP.Promise(function (resolve, reject) {
-      Ember.run.scheduleOnce('afterRender', resolve);
+      Ember.run.next(Ember.run.scheduleOnce, 'afterRender', resolve);
     });
   });
 }
