a5adf26a7267dfefb5b4ae50deed0ec067e90851
Stanley Stuart <sstuart@heroku.com>
2017-11-27 13:49:30 -0800

    update ember try and use yarn

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,5 +1,6 @@
 /*jshint node:true*/
 module.exports = {
+  useYarn: true,
   scenarios: [
     {
       name: 'default',
@@ -8,27 +9,18 @@ module.exports = {
     {
       name: 'ember-release',
       dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
         'ember': 'release'
       }
     },
     {
       name: 'ember-beta',
       dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
         'ember': 'beta'
-      }
+      },
     },
     {
       name: 'ember-canary',
       dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
         'ember': 'canary'
       }
     }
