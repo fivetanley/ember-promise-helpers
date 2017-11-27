877e1e52c291ddb70f2200894b64c485114c0719
Stanley Stuart <sstuart@heroku.com>
2017-11-27 14:29:48 -0800

    add test loader code

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,5 +2,8 @@ import resolver from './helpers/resolver';
 import {
   setResolver
 } from 'ember-qunit';
+import TestLoader from 'ember-cli-test-loader/test-support';
+
 
 setResolver(resolver);
+TestLoader.load();
